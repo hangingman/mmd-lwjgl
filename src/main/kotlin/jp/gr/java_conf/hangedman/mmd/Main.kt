dc6432fb183830d3893fe16673c60b14459f9f85
Hiroyuki-Nagata <newserver002@gmail.com>
2020-05-24 23:25:47 +0900

    Modify build.gradlew according to https://www.lwjgl.org/customize

--- src/main/kotlin/jp/gr/java_conf/hangedman/mmd/Main.kt
+++ src/main/kotlin/jp/gr/java_conf/hangedman/mmd/Main.kt
@@ -1,7 +1,9 @@
 package jp.gr.java_conf.hangedman.mmd
 
+import org.lwjgl.Version
+
 class Main
 
 fun main(args: Array<String>) {
-    println("Hello")
+    println("Hello, LWJGL - ${Version.getVersion()} !")
 }
