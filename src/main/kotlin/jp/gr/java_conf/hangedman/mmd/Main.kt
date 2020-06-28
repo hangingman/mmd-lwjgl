f8a30053d03b952743e17cf2f7fa930516575ae5
Hiroyuki-Nagata <newserver002@gmail.com>
2020-06-28 17:00:30 +0900

    Refactoring

--- src/main/kotlin/jp/gr/java_conf/hangedman/mmd/Main.kt
+++ src/main/kotlin/jp/gr/java_conf/hangedman/mmd/Main.kt
@@ -1,14 +1,16 @@
 package jp.gr.java_conf.hangedman.mmd
 
-import jp.gr.java_conf.hangedman.mmd.MmdLwjgl.Companion.cleanup
-import jp.gr.java_conf.hangedman.mmd.MmdLwjgl.Companion.initialize
-import jp.gr.java_conf.hangedman.mmd.MmdLwjgl.Companion.render
-import jp.gr.java_conf.hangedman.mmd.MmdLwjgl.Companion.update
+import jp.gr.java_conf.hangedman.mmd.MmdLwjglConstants.height
+import jp.gr.java_conf.hangedman.mmd.MmdLwjglConstants.title
+import jp.gr.java_conf.hangedman.mmd.MmdLwjglConstants.width
 import jp.gr.java_conf.hangedman.mmd.MmdLwjglOptionParser.parse
+import jp.gr.java_conf.hangedman.mmd.renderable_if.Renderable
 import org.lwjgl.Version
 import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWVidMode
 import org.lwjgl.opengl.GL33.GL_VERSION
 import org.lwjgl.system.Configuration
+import org.lwjgl.system.MemoryUtil.NULL
 
 
 fun main(args: Array<String>) {
@@ -21,18 +23,52 @@ fun main(args: Array<String>) {
     println("       OpenGL - ${GL_VERSION} !")
     println("       GLFW - ${glfwGetVersionString()} !")
 
+    // windowハンドラを得る
+    val windowId = initWindow()
+
     val modelPath = if (cmd.hasOption("m")) cmd.getOptionValue("m") else "HatsuneMiku.pmd"
     val pmdStruct = PmdLoader.loadPmdFile(modelPath)
-    val windowId = initialize(pmdStruct)
+
+    val renderables = listOf<Renderable>(
+            MmdLwjgl(windowId).initialize(pmdStruct)
+    )
 
     while (!glfwWindowShouldClose(windowId)) {
         glfwPollEvents()
-        update(windowId)
-        render()
+        renderables.forEach { it.update(windowId) }
+        renderables.forEach { it.render() }
         glfwSwapBuffers(windowId)
     }
     // GLFWの終了処理
-    cleanup()
+    renderables.forEach { it.cleanup() }
     glfwTerminate()
+}
+
+fun initWindow(): Long {
+    // GLFW初期化
+    glfwInit()
+    glfwDefaultWindowHints()
+    glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE)
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE)
+
+    // ウィンドウ生成
+    val windowId: Long = glfwCreateWindow(width, height, title, 0, 0)
+    if (windowId == NULL) {
+        // 生成に失敗
+        glfwTerminate()
+    }
+
+    glfwSetWindowAspectRatio(windowId, 1, 1)
+    val videoMode: GLFWVidMode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+            ?: throw IllegalStateException("Failed to get video mode...")
+    glfwSetWindowPos(windowId,
+            (videoMode.width() - width) / 2,
+            (videoMode.height() - height) / 2
+    )
+    return windowId
 }
 
---
+++ src/main/kotlin/jp/gr/java_conf/hangedman/mmd/renderable_if/Renderable.kt
@@ -0,0 +1,11 @@
+package jp.gr.java_conf.hangedman.mmd.renderable_if
+
+import jp.gr.java_conf.hangedman.mmd.pmd.PmdStruct
+
+interface Renderable {
+    val windowId: Long
+    fun initialize(pmdStruct: PmdStruct): Renderable
+    fun update(windowId: Long)
+    fun render()
+    fun cleanup()
+}
